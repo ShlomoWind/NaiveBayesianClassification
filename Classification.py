d4719a04a46663881fd27908713d3a0c913d70ac
Shlomo <sh.z.wind@gmail.com>
2025-07-08 18:09:56 +0300

    Changed the logical of the code

--- Classification.py
+++ Classification.py
@@ -1,19 +1,29 @@
 import pandas as pd
 
 df = pd.read_csv('buy_computer_data.csv')
+
 classes = df['buys_computer'].unique()
-classes_subset = {}
+
+class_probs = {}
+total_count = len(df)
 
 for cls in classes:
-    subset = df[df['buys_computer'] == cls]
-    classes_subset[cls] = subset
+    class_count = len(df[df['buys_computer'] == cls])
+    class_probs[cls] = class_count / total_count
 
 probabilities = {}
-for cls,subset in classes_subset.items():
-    column_dict = {}
-    for column in subset.columns:
-        if column == 'buys_computer':
-            continue
-        value_count = subset[column].value_counts(normalize=True)
-        column_dict[column] = value_count.to_dict()
-    probabilities[cls] = column_dict
+
+for column in df.columns:
+    if column == 'buys_computer':
+        continue
+    grouped = df.groupby(['buys_computer', column]).size()
+    cls_count = df['buys_computer'].value_counts()
+    column_values = df[column].unique()
+    probabilities[column] = {}
+    for value in column_values:
+        probabilities[column][value] = {}
+        for cls in classes:
+            count = grouped.get((cls, value),0)
+            total = cls_count[cls]
+            prob = count / total
+            probabilities[column][value][cls] = prob
