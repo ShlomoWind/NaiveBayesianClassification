096055a469719d94375a287f589f2ca5ff8604d3
Shlomo <sh.z.wind@gmail.com>
2025-07-09 18:08:14 +0300

    I wrote the test function

---
+++ testing_module/Tester.py
@@ -0,0 +1,13 @@
+from random import sample
+
+
+def accuracy_check(test_df, predict, class_probs, probabilities):
+    correct_counter = 0
+    total = len(test_df)
+    for index, row in test_df.iterrows():
+        sample = row.to_dict()
+        predicted = predict(sample,class_probs,probabilities)
+        if predicted == index:
+            correct_counter += 1
+    return correct_counter / len(test_df)
+
