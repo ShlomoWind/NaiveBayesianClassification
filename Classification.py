905f1c650ae3fc82206a9cd9d7f0c38791a653d4
Shlomo <sh.z.wind@gmail.com>
2025-07-09 10:09:45 +0300

    added the prediction function

--- Classification.py
+++ Classification.py
@@ -4,14 +4,14 @@ df = pd.read_csv('buy_computer_data.csv')
 
 classes = df['buys_computer'].unique()
 
-class_probs = {}
+class_probs = {} #General probabilities of the categories (yes, no)
 total_count = len(df)
 
 for cls in classes:
     class_count = len(df[df['buys_computer'] == cls])
     class_probs[cls] = class_count / total_count
 
-probabilities = {}
+probabilities = {} #Conditional probabilities of each value for each column in each category
 
 for column in df.columns:
     if column == 'buys_computer':
@@ -27,3 +27,15 @@ for column in df.columns:
             total = cls_count[cls]
             prob = count / total
             probabilities[column][value][cls] = prob
+
+def predict(sample,class_probs,probabilities):
+    scores = {}
+    for cls in class_probs:
+        prob = class_probs[cls]
+        for feature,value in sample.items():
+            if feature in probabilities and value in probabilities[feature]:
+                prob *= probabilities[feature][value].get(cls,1e-6)
+            else:
+                prob *= 1e-6
+        scores[cls] = prob
+    return max(scores, key=scores.get)
