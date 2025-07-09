54564a0ae9574c1f5c5968e868e799bcc6bb2fb0
Shlomo <sh.z.wind@gmail.com>
2025-07-09 15:23:12 +0300

    i createthe functions on the user_input file

--- user_module/user_input.py
+++ user_module/user_input.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def data_info():
-    data_path = input("").strip()
-    data_type = input("").strip().lower()
+    data_path = input("Enter path to data file: ").strip()
+    data_type = input("Enter data source type: ").strip().lower()
     return data_path,data_type
 
 def column_for_prediction(data_frame):
